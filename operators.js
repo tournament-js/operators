92aafc0eb06317870285b730d470458751b25420
clux <analsandblaster@gmail.com>
2016-01-24 12:58:41 +0000

    Rewrite for ES6
    This module is not used much anyway apart from in interlude which will likely not pull in newer versions of operators. If it does, it will be under a new major as there will likely be some changes.

--- operators.js
+++ operators.js
@@ -1,102 +1,34 @@
 var $ = {}
   , concat = Array.prototype.concat;
 
-// multi-parameter versions for the associative operators:
-$.plus2 = function (x, y) {
-  return x + y;
-};
-$.plus3 = function (x, y, z) {
-  return x + y + z;
-};
-$.plus4 = function (x, y, z, w) {
-  return x + y + z + w;
-};
+// multi-parameter versions for the associative operators
+$.plus2 = (x, y) => x + y;
+$.plus3 = (x, y, z) => x + y + z;
+$.plus4 = (x, y, z, w) => x + y + z + w;
+$.times2 = (x, y) => x * y;
+$.times3 = (x, y, z) => x * y * z;
+$.times4 = (x, y, z, w) => x * y * z * w;
+$.and2 = (x, y) => x && y;
+$.and3 = (x, y, z) => x && y && z;
+$.and4 = (x, y, z, w) => x && y && z && w;
+$.or2 = (x, y) => x || y;
+$.or3 = (x, y, z) => x || y || z;
+$.or4 = (x, y, z, w) => x || y || z || w;
 
-$.times2 = function (x, y) {
-  return x * y;
-};
-$.times3 = function (x, y, z) {
-  return x * y * z;
-};
-$.times4 = function (x, y, z, w) {
-  return x * y * z * w;
-};
+// same, but on arrays
+$.append2 = (xs, ys) => xs.concat(ys);
+$.append3 = (xs, ys, zs) => xs.concat(ys, zs);
+$.append4 = (xs, ys, zs, ws) => xs.concat(ys, zs, ws);
+$.prepend2 = (xs, ys) => ys.concat(xs);
 
-$.and2 = function (x, y) {
-  return x && y;
-};
-$.and3 = function (x, y, z) {
-  return x && y && z;
-};
-$.and4 = function (x, y, z, w) {
-  return x && y && z && w;
-};
+$.sum = (xs) => xs.reduce((acc, el) => acc + el, 0);
+$.product = (xs) => xs.reduce((acc, el) => acc * el, 1);
+$.and = (xs) => xs.reduce((acc, el) => acc && el, true);
+$.or = (xs) => xs.reduce((acc, el) => acc || el, false);
 
-$.or2 = function (x, y) {
-  return x || y;
-};
-$.or3 = function (x, y, z) {
-  return x || y || z;
-};
-$.or4 = function (x, y, z, w) {
-  return x || y || z || w;
-};
+$.flatten = (xs) => concat.apply([], xs);
 
-$.append2 = function (xs, ys) {
-  return xs.concat(ys);
-};
-$.append3 = function (xs, ys, zs) {
-  return xs.concat(ys, zs);
-};
-$.append4 = function (xs, ys, zs, ws) {
-  return xs.concat(ys, zs, ws);
-};
-$.prepend2 = function (xs, ys) {
-  return ys.concat(xs);
-};
-
-// Array versions of 5/6 associative operators by looping rather than reducing ops
-// Associative reductions would make these functions one-liners..
-// I.e. $.product = (xs) -> xs.reduce($.times2, 1)
-// These abstractions are very much not free however, and are thus avoided for now:
-// http://jsperf.com/reduce-vs-slicereduce3
-$.sum = function (xs) {
-  var sum = 0;
-  for (var i = 0; i < xs.length; i += 1) {
-    sum += xs[i];
-  }
-  return sum;
-};
-
-$.product = function (xs) {
-  var product = 1;
-  for (var i = 0, len = xs.length; i < len; i += 1) {
-    product *= xs[i];
-  }
-  return product;
-};
-
-$.and = function (xs) {
-  var and = true;
-  for (var i = 0, len = xs.length; i < len; i += 1) {
-    and = and && xs[i];
-  }
-  return and;
-};
-
-$.or = function (xs) {
-  var or = false;
-  for (var i = 0, len = xs.length; i < len; i += 1) {
-    or = or || xs[i];
-  }
-  return or;
-};
-
-$.flatten = function (xs) {
-  return concat.apply([], xs);
-};
-
-// variadic versions
+// variadic versions - may cause deoptimizations
 $.add = function () {
   var sum = 0;
   for (var i = 0, len = arguments.length; i < len; i += 1) {
@@ -118,147 +50,36 @@ $.concat = function () {
 };
 
 // non-associative operators only get the 2 argument version
-$.minus2 = function (x, y) {
-  return x - y;
-};
-
-$.divide2 = function (x, y) {
-  return x / y;
-};
-
-$.div2 = function (x, y) {
-  return Math.floor(x / y);
-};
-
-$.mod2 = function (x, y) {
-  return x % y;
-};
-
+$.minus2 = (x, y) => x - y;
+$.divide2 = (x, y) => x / y;
+$.div2 = (x, y) => Math.floor(x / y);
+$.mod2 = (x, y) => x % y;
 $.pow2 = Math.pow;
 
-$.log2 = function (x, y) {
-  return Math.log(x) / Math.log(y);
-};
-
-$.eq2 = function (x, y) {
-  return x === y;
-};
-
-$.neq2 = function (x, y) {
-  return x !== y;
-};
-
-$.gt2 = function (x, y) {
-  return x > y;
-};
-
-$.lt2 = function (x, y) {
-  return x < y;
-};
-
-$.gte2 = function (x, y) {
-  return x >= y;
-};
-
-$.lte2 = function (x, y) {
-  return x <= y;
-};
+$.log2 = (x, y) => Math.log(x) / Math.log(y);
+$.eq2 = (x, y) => x === y;
+$.neq2 = (x, y) => x !== y;
+$.gt2 = (x, y) => x > y;
+$.lt2 = (x, y) => x < y;
+$.gte2 = (x, y) => x >= y;
+$.lte2 = (x, y) => x <= y;
 
 // curried versions
-$.plus = function (y) {
-  return function (x) {
-    return x + y;
-  };
-};
-
-$.minus = function (y) {
-  return function (x) {
-    return x - y;
-  };
-};
-
-$.times = function (y) {
-  return function (x) {
-    return x * y;
-  };
-};
-
-$.divide = function (y) {
-  return function (x) {
-    return x / y;
-  };
-};
-
-$.div = function (y) {
-  return function (x) {
-    return Math.floor(x / y);
-  };
-};
-
-$.mod = function (y) {
-  return function (x) {
-    return x % y;
-  };
-};
-
-$.pow = function (y) {
-  return function (x) {
-    return Math.pow(x, y);
-  };
-};
-
-$.log = function (y) {
-  return function (x) {
-    return Math.log(x) / Math.log(y);
-  };
-};
-
-$.append = function (ys) {
-  return function (xs) {
-    return xs.concat(ys);
-  };
-};
-
-$.prepend = function (ys) {
-  return function (xs) {
-    return ys.concat(xs);
-  };
-};
-
-$.gt = function (y) {
-  return function (x) {
-    return x > y;
-  };
-};
-
-$.lt = function (y) {
-  return function (x) {
-    return x < y;
-  };
-};
-
-$.eq = function (y) {
-  return function (x) {
-    return x === y;
-  };
-};
-
-$.neq = function (y) {
-  return function (x) {
-    return x !== y;
-  };
-};
-
-$.gte = function (y) {
-  return function (x) {
-    return x >= y;
-  };
-};
-
-$.lte = function (y) {
-  return function (x) {
-    return x <= y;
-  };
-};
+$.plus = (y) => (x) => x + y;
+$.minus = (y) => (x) => x - y;
+$.times = (y) => (x) => x * y;
+$.divide = (y) => (x) => x / y;
+$.div = (y) => (x) => Math.floor(x / y);
+$.mod = (y) => (x) => x % y;
+$.pow = (y) => (x) => Math.pow(x, y);
+$.log = (y) => (x) => Math.log(x) / Math.log(y);
+$.append = (ys) => (xs) => xs.concat(ys);
+$.prepend = (ys) => (xs) => ys.concat(xs);
+$.gt = (y) => (x) => x > y;
+$.lt = (y) => (x) => x < y;
+$.eq = (y) => (x) => x === y;
+$.neq = (y) => (x) => x !== y;
+$.gte = (y) => (x) => x >= y;
+$.lte = (y) => (x) => x <= y;
 
 module.exports = $;
--- test/optest.js
+++ test/optest.js
@@ -1,116 +1,112 @@
-var $ = require(process.env.OPERATORS_COV ? '../operators-cov.js' : '../');
+var $ = require('..');
+var test = require('bandage');
 
-exports.basicUncurried = function (t) {
-  t.eql = t.deepEqual;
+test('basic uncurried', function *(t) {
+  t.eq($.plus2(2,1), 3, '2+1');
+  t.eq($.plus3(3,2,1), 6, '3+2+1');
+  t.eq($.plus4(4,3,2,1), 10, '4+3+2+1');
 
-  t.equal($.plus2(2,1), 3, "2+1");
-  t.equal($.plus3(3,2,1), 6, "3+2+1");
-  t.equal($.plus4(4,3,2,1), 10, "4+3+2+1");
+  t.eq($.times2(3,2), 6, '3*2');
+  t.eq($.times3(4,3,2), 24, '4*3*2');
+  t.eq($.times4(5,4,3,2), 120, '5*4*3*2');
 
-  t.equal($.times2(3,2), 6, "3*2");
-  t.equal($.times3(4,3,2), 24, "4*3*2");
-  t.equal($.times4(5,4,3,2), 120, "5*4*3*2");
+  t.eq($.and([1,1,1]), 1, 'and map true');
+  t.eq($.and2(1,1), 1, '1 && 1');
+  t.eq($.and3(1,0,1), 0, '1 && 0 && 1');
+  t.eq($.and4(1,1,1,1), 1, '1 && 1 && 1 && 1');
 
-  t.equal($.and([1,1,1]), 1, "and map true");
-  t.equal($.and2(1,1), 1, "1 && 1");
-  t.equal($.and3(1,0,1), 0, "1 && 0 && 1");
-  t.equal($.and4(1,1,1,1), 1, "1 && 1 && 1 && 1");
-
-  t.equal($.or([0,0,0]), 0, "or map false");
-  t.equal($.or2(0,1), 1, "0 || 1");
-  t.equal($.or3(0,0,0), 0, "0 || 0 || 0");
-  t.equal($.or4(0,0,0,1), 1, "0 || 0 || 0 || 1");
+  t.eq($.or([0,0,0]), 0, 'or map false');
+  t.eq($.or2(0,1), 1, '0 || 1');
+  t.eq($.or3(0,0,0), 0, '0 || 0 || 0');
+  t.eq($.or4(0,0,0,1), 1, '0 || 0 || 0 || 1');
 
   // made full and/or tests for some reason
-  t.equal($.and2(false, false), false, "false && false === false");
-  t.equal($.and2(true, false), false, "true && false === false");
-  t.equal($.and2(false, true), false, "false && true === false");
-  t.equal($.and2(true, true), true, "true && true === true");
-  t.equal($.or2(false, false), false, "false || false === false");
-  t.equal($.or2(true, false), true, "true || false === true");
-  t.equal($.or2(false, true), true, "false || true === true");
-  t.equal($.or2(true, true), true, "true || true === true");
+  t.eq($.and2(false, false), false, 'false && false === false');
+  t.eq($.and2(true, false), false, 'true && false === false');
+  t.eq($.and2(false, true), false, 'false && true === false');
+  t.eq($.and2(true, true), true, 'true && true === true');
+  t.eq($.or2(false, false), false, 'false || false === false');
+  t.eq($.or2(true, false), true, 'true || false === true');
+  t.eq($.or2(false, true), true, 'false || true === true');
+  t.eq($.or2(true, true), true, 'true || true === true');
 
-  t.eql($.prepend2([1,2], [3]), [3,1,2], "prepend2([1, 2], [3]) === [3,1,2]");
-  t.eql($.append2([1,2], [3]), [1,2,3], "prepend2([1, 2], [3]) === [1,2,3]");
-  t.eql($.append3([1,2], [3], [4,5]), [1,2,3,4,5], "prepend3([1,2], [3], [4,5]) === [1,2,3,4,5]");
-  t.eql($.append4([1], [2], [[3]], [4]), [1,2,[3],4], "prepend3([1],[2],[[3]],[4]) === [1,2,[3],4]");
+  t.eq($.prepend2([1,2], [3]), [3,1,2], 'prepend2([1, 2], [3]) === [3,1,2]');
+  t.eq($.append2([1,2], [3]), [1,2,3], 'prepend2([1, 2], [3]) === [1,2,3]');
+  t.eq($.append3([1,2], [3], [4,5]), [1,2,3,4,5], 'prepend3([1,2], [3], [4,5]) === [1,2,3,4,5]');
+  t.eq($.append4([1], [2], [[3]], [4]), [1,2,[3],4], 'prepend3([1],[2],[[3]],[4]) === [1,2,[3],4]');
 
   // non associative bunch
-  t.equal($.minus2(3,2), 1, "3-2 === 1");
-  t.equal($.divide2(9,3), 3, "9/3 === 3");
-  t.equal($.div2(7,2), 3, "7 div 2 === 3");
-  t.equal($.mod2(12,5), 2, "12 mod 5 === 2");
+  t.eq($.minus2(3,2), 1, '3-2 === 1');
+  t.eq($.divide2(9,3), 3, '9/3 === 3');
+  t.eq($.div2(7,2), 3, '7 div 2 === 3');
+  t.eq($.mod2(12,5), 2, '12 mod 5 === 2');
 
-  t.equal($.pow2(2, 3), 8, "2^3 === 8");
-  t.equal($.pow2(3, 0), 1, "3^0 === 1");
-  t.equal($.pow2(4, 2), 16, "4^2 === 16");
-  t.equal($.log2(16, 2), 4, "4 is log2 of 16");
-  t.equal($.log2(100, 10), 2, "2 is log10 of 1000");
+  t.eq($.pow2(2, 3), 8, '2^3 === 8');
+  t.eq($.pow2(3, 0), 1, '3^0 === 1');
+  t.eq($.pow2(4, 2), 16, '4^2 === 16');
+  t.eq($.log2(16, 2), 4, '4 is log2 of 16');
+  t.eq($.log2(100, 10), 2, '2 is log10 of 1000');
 
-  t.equal($.eq2(1,1), true, "1 === 1");
-  t.equal($.eq2(1,"1"), false, "1 === '1'");
-  t.equal($.neq2(1,1), false, "!(1 !== 1)");
-  t.equal($.neq2(1,2), true, "(1 !== 2)");
-  t.equal($.gt2(3,2), true, "3 > 2");
-  t.equal($.gt2(3,3), false, "3 ! > 2");
-  t.equal($.lt2(2,3), true, "2 < 3");
-  t.equal($.lt2(3,3), false, "3 ! < 3");
-  t.equal($.gte2(3,3), true, "3 >= 3");
-  t.equal($.gte2(2,3), false, "2 ! >= 3");
-  t.equal($.lte2(2,2), true, "2 <= 2");
-  t.equal($.lte2(2,1), false, "2 ! <= 1");
-  t.done();
-};
+  t.eq($.eq2(1,1), true, '1 === 1');
+  t.eq($.eq2(1,'1'), false, '1 === "1"');
+  t.eq($.neq2(1,1), false, '!(1 !== 1)');
+  t.eq($.neq2(1,2), true, '(1 !== 2)');
+  t.eq($.gt2(3,2), true, '3 > 2');
+  t.eq($.gt2(3,3), false, '3 ! > 2');
+  t.eq($.lt2(2,3), true, '2 < 3');
+  t.eq($.lt2(3,3), false, '3 ! < 3');
+  t.eq($.gte2(3,3), true, '3 >= 3');
+  t.eq($.gte2(2,3), false, '2 ! >= 3');
+  t.eq($.lte2(2,2), true, '2 <= 2');
+  t.eq($.lte2(2,1), false, '2 ! <= 1');
+});
 
-exports.curried = function (t) {
-  t.equal($.plus(3)(2), 5, "2+3 === 5");
-  t.equal($.minus(3)(5), 2, "5-3 === 2");
-  t.equal($.times(2)(3), 6, "2*3 === 6");
-  t.equal($.divide(2)(6), 3, "6/2 === 3");
-  t.equal($.div(2)(7), 3, "7 div 2 === 3");
-  t.equal($.mod(5)(11), 1, "11 mod 5 === 1");
+test('curried', function *(t) {
+  t.eq($.plus(3)(2), 5, '2+3 === 5');
+  t.eq($.minus(3)(5), 2, '5-3 === 2');
+  t.eq($.times(2)(3), 6, '2*3 === 6');
+  t.eq($.divide(2)(6), 3, '6/2 === 3');
+  t.eq($.div(2)(7), 3, '7 div 2 === 3');
+  t.eq($.mod(5)(11), 1, '11 mod 5 === 1');
 
-  t.equal($.pow(3)(2), 8, "2^3 === 8");
-  t.equal($.pow(0)(3), 1, "3^0 === 1");
-  t.equal($.pow(2)(4), 16, "4^2 === 16");
-  t.equal($.log(2)(16), 4, "4 is log2 of 16");
-  t.equal($.log(10)(100), 2, "2 is log10 of 1000");
+  t.eq($.pow(3)(2), 8, '2^3 === 8');
+  t.eq($.pow(0)(3), 1, '3^0 === 1');
+  t.eq($.pow(2)(4), 16, '4^2 === 16');
+  t.eq($.log(2)(16), 4, '4 is log2 of 16');
+  t.eq($.log(10)(100), 2, '2 is log10 of 1000');
 
-  t.deepEqual($.append([1,2])([3]), [3,1,2], "(++[1, 2]) [3] === [3, 1, 2]");
-  t.deepEqual($.prepend([1,2])([3]), [1,2,3], "([1, 2]++) [3] === [1, 2, 3]");
+  t.deepEqual($.append([1,2])([3]), [3,1,2], '(++[1, 2]) [3] === [3, 1, 2]');
+  t.deepEqual($.prepend([1,2])([3]), [1,2,3], '([1, 2]++) [3] === [1, 2, 3]');
 
-  t.equal($.gt(5)(5), false, "5 ! > 5");
-  t.equal($.gt(5)(6), true, "6 > 5");
-  t.equal($.gt(5)(4), false, "4 ! > 5");
+  t.eq($.gt(5)(5), false, '5 ! > 5');
+  t.eq($.gt(5)(6), true, '6 > 5');
+  t.eq($.gt(5)(4), false, '4 ! > 5');
 
-  t.equal($.gte(5)(5), true, "5 >= 5");
-  t.equal($.gte(5)(6), true, "6 >= 5");
-  t.equal($.gte(5)(4), false, "4 ! >= 5");
+  t.eq($.gte(5)(5), true, '5 >= 5');
+  t.eq($.gte(5)(6), true, '6 >= 5');
+  t.eq($.gte(5)(4), false, '4 ! >= 5');
 
-  t.equal($.lt(5)(5), false, "5 ! < 5");
-  t.equal($.lt(5)(6), false, "6 ! < 5");
-  t.equal($.lt(5)(4), true, "4 < 5");
+  t.eq($.lt(5)(5), false, '5 ! < 5');
+  t.eq($.lt(5)(6), false, '6 ! < 5');
+  t.eq($.lt(5)(4), true, '4 < 5');
 
-  t.equal($.lte(5)(5), true, "5 <= 5");
-  t.equal($.lte(5)(6), false, "6 ! <= 5");
-  t.equal($.lte(5)(4), true, "4 <= 5");
+  t.eq($.lte(5)(5), true, '5 <= 5');
+  t.eq($.lte(5)(6), false, '6 ! <= 5');
+  t.eq($.lte(5)(4), true, '4 <= 5');
 
-  t.equal($.eq(5)(5), true, "5 === 5");
-  t.equal($.lt(5)('5'), false, "5 !== '5'");
+  t.eq($.eq(5)(5), true, '5 === 5');
+  t.eq($.lt(5)('5'), false, '5 !== "5"');
 
-  t.equal($.neq(4)(4), false, "!(4 !== 4)");
-  t.equal($.neq(4)(5), true, "(4 !== 5)");
-  t.done();
-};
+  t.eq($.neq(4)(4), false, '!(4 !== 4)');
+  t.eq($.neq(4)(5), true, '(4 !== 5)');
+});
 
-exports.lifted = function (t) {
-  t.equal($.sum([1,2,3,4]), 10, "sum [1,2,3,4] === 10");
-  t.equal($.product([1,2,3,4]), 24, "product [1,2,3,4] === 24");
-  t.deepEqual($.flatten([ [1,2,3], [[4]], [5] ]), [1,2,3,[4],5], "$.flatten");
+test('lifted', function *(t) {
+  t.eq($.sum([1,2,3,4]), 10, 'sum [1,2,3,4] === 10');
+  t.eq($.product([1,2,3,4]), 24, 'product [1,2,3,4] === 24');
+  t.deepEqual($.flatten([ [1,2,3], [[4]], [5] ]), [1,2,3,[4],5], '$.flatten');
 
-  t.equal($.add(1,2,3,4), 10, "add(1,2,3,4) === 10");
-  t.equal($.multiply(1,2,3,4), 24, "multiply(1,2,3,4) === 24");
-  t.deepEqual($.concat([1,2,3], [[4]], [5]), [1,2,3,[4],5], "$.concat");
-  t.done();
-};
+  t.eq($.add(1,2,3,4), 10, 'add(1,2,3,4) === 10');
+  t.eq($.multiply(1,2,3,4), 24, 'multiply(1,2,3,4) === 24');
+  t.deepEqual($.concat([1,2,3], [[4]], [5]), [1,2,3,[4],5], '$.concat');
+});
